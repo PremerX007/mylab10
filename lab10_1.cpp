184175676bdbb3802460c043ca7f4585d3dafee6
PremerX007 <39229888+PremerX007@users.noreply.github.com>
2022-12-27 15:47:19 +0700

    lab10_1.cpp completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double loan, rate, amount, interest, total, newcost=0;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> amount;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,23 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << fixed << setprecision(2);
+	int i=1;
+	while(true){
+		cout << setw(13) << left << i;
+		cout << setw(13) << left << loan;
+		interest = loan*(rate/100);
+		cout << setw(13) << left << interest;
+		total = loan+interest;
+		cout << setw(13) << left << total;
+		if(total<=amount) amount = total;
+		cout << setw(13) << left << amount;
+		newcost = total-amount;
+		cout << setw(13) << left << newcost;
+		loan = newcost;
+		cout << endl;
+		i++;
+		if(newcost==0) break;
+	}
 	return 0;
 }
